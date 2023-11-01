1409110f45198a832445ea4e89980d981566d13c
GavinBlackvt <gavin.g@blackvt.com>
2023-11-01 12:26:38 +0530

    join with us api

---
+++ src/api/common-join-with-us/controllers/common-join-with-us.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * common-join-with-us controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::common-join-with-us.common-join-with-us');
---
+++ src/api/common-join-with-us/routes/common-join-with-us.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * common-join-with-us router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::common-join-with-us.common-join-with-us');
