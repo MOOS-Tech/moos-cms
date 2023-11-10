b5f7fc24bd70e2da7a4b28bbd3a06a99fab0754a
GavinBlackvt <gavin.g@blackvt.com>
2023-11-10 11:27:02 +0530

    Update:add new element to navbar

---
+++ src/api/navbar-right-side/routes/navbar-right-side.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * navbar-right-side router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::navbar-right-side.navbar-right-side');
