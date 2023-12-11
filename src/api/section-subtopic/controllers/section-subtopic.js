bbf39529629d40eb262bed578659ff155615ee3c
GavinBlackvt <gavin.g@blackvt.com>
2023-12-11 11:54:51 +0530

    rearrange titles

---
+++ src/api/section-subtopic/controllers/section-subtopic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * section-subtopic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::section-subtopic.section-subtopic');
