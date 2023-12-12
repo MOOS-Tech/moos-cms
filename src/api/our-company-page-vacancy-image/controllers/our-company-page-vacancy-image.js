72186f708a8f9f041adb71856f86fc24bfbccce0
GavinBlackvt <gavin.g@blackvt.com>
2023-12-12 14:26:30 +0530

    vacancy page image

---
+++ src/api/our-company-page-vacancy-image/controllers/our-company-page-vacancy-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * our-company-page-vacancy-image controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::our-company-page-vacancy-image.our-company-page-vacancy-image');
